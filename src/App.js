0e76e8552f1e049514aaecf8891c9e475901ac0c
DriscollRichie <driscollrichie@gmail.com>
2018-03-03 16:07:25 -0700

    Reviewed mini proj for practice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+   constructor() {
+      super()
+
+      this.state = {
+         picture: '',
+         name: '',
+         friends: [] 
+      }
+
+      this.updatePicture = this.updatePicture.bind(this)
+      this.updateName = this.updateName.bind(this)
+      this.addFriend = this.addFriend.bind(this)
+   }
+
+   updatePicture(e) {
+      this.setState({picture: e.target.value}) 
+   }
+
+   updateName(e) {
+      this.setState({name: e.target.value})
+   }
+   addFriend() {
+      var newFriends = this.state.friends.slice()
+       console.log(newFriends)
+       newFriends.push({name: this.state.name, picture: this.state.picture})
+       console.log(newFriends)
+       this.setState({picture: '', name: '', friends: newFriends})
+   }
+
   render() {
+     const friends = this.state.friends.map((val, i) => {
+      //   console.log('friends val',val)
+        return(
+           <div key={i}>
+              <img src={val.picture} />
+              <span> {val.name} </span>
+           </div>
+        )
+     })
+     console.log(this.state)
+     console.log(friends)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+       <div class="taco">
+          <span> Picture </span>
+          <input onChange={this.updatePicture} value={this.state.picture} />
+          <span> Name </span>
+          <input onChange={this.updateName} value={this.state.name}/>
+          <button onClick={this.addFriend}> Add Friend </button>
+          {friends}
+         
+
+       </div>
     );
   }
 }
