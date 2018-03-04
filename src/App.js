e581f447747de81beebc07de57e316c001674768
DriscollRichie <driscollrichie@gmail.com>
2018-03-03 17:57:42 -0700

    Finished 2nd attempt

--- src/App.js
+++ src/App.js
@@ -3,17 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+   constructor() {
+      super()
+
+      this.state = {
+         name: '',
+         picture: '',
+         friends: []
+      }
+      this.updatePicture = this.updatePicture.bind(this)
+      this.updateName = this.updateName.bind(this)
+      this.addFriend = this.addFriend.bind(this)
+   }
+   updatePicture(e){
+      this.setState({picture: e.target.value})
+   }
+   updateName(e){
+      this.setState({name: e.target.value})
+   }
+   addFriend(){
+      var newFriends = this.state.friends.slice()
+      newFriends.push({name: this.state.name, picture: this.state.picture})
+      this.setState({picture: '', name: '', friends: newFriends})
+   }
+
   render() {
+     console.log(this.state.friends)
+     const friends = this.state.friends.map((val, i) => {
+        console.log(val)
+        return (
+           <div key={i}>
+              <img src={val.picture} />
+              <span>{val.name}</span>
+           </div>
+        )
+     })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+       <div>
+          <span> Picture </span>
+          <input onChange={this.updatePicture} value={this.state.picture} />
+          <span> Name </span>
+          <input onChange={this.updateName} value={this.state.name}/>
+          <button onClick={this.addFriend}> Add Friend </button>
+          {friends}
+       </div>
     );
   }
 }
